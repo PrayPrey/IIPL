39824075faa3b5edd06367bac7e57774715942c5
wooyoonkyu <yoon303b@gmail.com>
2025-03-22 13:58:55 +0900

    additional_custom

--- calculate.py
+++ calculate.py
@@ -14,6 +14,8 @@ def divide(a,b):
 def custom(a,b):
     return a//b
 
+def custom2(a,b):
+    return a%b
 
 
 
@@ -24,7 +26,7 @@ if __name__ == '__main__':
     input1 = input('입력 :')
 
 
-    print('\n원하는 사칙연산 기호 중 하나를 선택하세요. (+, -, *, /)')
+    print('\n원하는 사칙연산 기호 중 하나를 선택하세요. (+, -, *, /, //, %)')
     act = input('기호 : ')
 
     print('\n두 번째 숫자를 입력하세요.')
@@ -42,6 +44,8 @@ if __name__ == '__main__':
         result = divide(input1, input2)
     elif act == '//':
         result = custom(input1, input2)
+    elif act == "%":
+        result = custom2(input1, input2)
 
 
     print(f"사칙연산 결과는 {result}입니다.")
